019ec941c594e679880bc63788cff3e8ed48e103
Armin Versteeg <armin.versteeg@outlook.com>
2026-02-15 19:12:58 +0100

    Added all Eagles

--- stratagems.py
+++ stratagems.py
@@ -19,11 +19,36 @@ STRATAGEMS = {
     # Eagles
     # ==========================================
     ("up", "right", "down", "down", "down"): {
-        "name": "500KG Bomb",
+        "name": "Eagle 500KG Bomb",
         "image": "images/stratagems/500kg.png",
     },
     ("up", "right", "right"): {
         "name": "Eagle Strafing Run",
-        "image": "images/stratagems/eagle_strafing_run.png"
+        "image": "images/stratagems/eagle_strafing_run.png",
     },
+    ("up", "right", "down", "right"): {
+        "name": "Eagle Airstrike",
+        "image": "images/stratagems/eagle_airstrike.png",
+    },
+    ("up", "right", "down", "down", "right"): {
+        "name": "Eagle Cluster Bomb",
+        "image": "images/stratagems/eagle_cluster_bomb.png",
+    },
+    ("up", "right", "down", "up"): {
+        "name": "Eagle Napalm Airstrike",
+        "image": "images/stratagems/eagle_napalm_airstrike.png",
+    },
+    ("up", "right", "up", "down"): {
+        "name": "Eagle Smoke Strike",
+        "image": "images/stratagems/eagle_smoke_strike.png",
+    },
+    ("up", "right", "up", "left"): {
+        "name": "Eagle 110mm Rocket Pods",
+        "image": "images/stratagems/eagle_110mm.png",
+    },
+
+    # ==========================================
+    # Orbitals
+    # ==========================================
+    
 }
